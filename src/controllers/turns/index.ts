152260e3d2b1c4fc9c67b303d79505b9cfb97a1d
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-12-01 13:07:53 -0300

    [test] dates get turns v3

--- src/controllers/turns/index.ts
+++ src/controllers/turns/index.ts
@@ -66,7 +66,7 @@ export const getTurns = {
     next: NextFunction
   ): Promise<void> => {
     const { id } = req.params;
-     console.log("dates", moment(), moment().set({hour: 0, minutes: 0}).utc().utcOffset(3, true).toDate(), moment().set({hour: 23, minutes: 59}).utc().utcOffset(3, true).toDate())
+     console.log("dates", moment(), moment().utc().utcOffset(3, true).set({hour: 0, minutes: 0}).toDate(), moment().utc().utcOffset(3, true).set({hour: 23, minutes: 0}).toDate())
     try {
       const turns = await Turn.aggregate([
         {
