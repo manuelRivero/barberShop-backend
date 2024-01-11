991c6c6f8c0912f7733c9f1bae44db3b455c45b3
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-01-11 16:22:58 -0300

    [add] barberDetail endpoint

--- src/controllers/barbers/index.ts
+++ src/controllers/barbers/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const getBarbers = {
     do: async (req: Request, res: Response) => {
         const barbers = await user.aggregate([{
-          $match:{role: "barber"}
+            $match: { role: "barber" }
         }]);
         console.log("barbers", barbers);
         res.json({
@@ -14,3 +14,26 @@ export const getBarbers = {
         });
     },
 };
+
+export const getBarberDetail = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const barber = await user.aggregate([{
+        $match: { _id: new mongoose.Types.ObjectId(id) }
+    },
+    {
+        $project: {
+            name: 1,
+            lastname: 1,
+            email: 1,
+            image: 1,
+            role: 1,
+            bio: 1
+        }
+    }]);
+    console.log("barbers", barber);
+    res.json({
+        ok: true,
+        barber,
+    });
+}
+
--- src/routes/barbers/index.ts
+++ src/routes/barbers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getBarbers } from "../../controllers/barbers/index";
+import { getBarberDetail, getBarbers } from "../../controllers/barbers/index";
 import { validateJWT } from "../../middleware/validateJWT/index";
 
 const router = Router();
 
 // router.post('/register', register.check, register.do)
 router.get("/", validateJWT, getBarbers.do);
+
+router.get("/:id", validateJWT, getBarberDetail)
 
 export default router;
