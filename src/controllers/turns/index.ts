e2c7d081027619e44d80add0f28f7e9bbb30d227
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-01-15 21:58:38 -0300

    test active turn dates

--- src/controllers/turns/index.ts
+++ src/controllers/turns/index.ts
@@ -144,6 +144,8 @@ export const getTurnDetail = {
 export const getActiveTurn = {
   do: async (req: Request, res: Response): Promise<void> => {
     const { uid } = req
+    console.log("date", moment.tz('America/Argentina/Buenos_Aires')
+      .set({ hour: 0, minutes: 0 }))
     const turn = await Turn.aggregate([
       {
         $match: {
