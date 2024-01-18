5fce519c3411516f901fe98f4a7c6b40a1a8b1a8
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-01-18 15:56:10 -0300

    [add] refresh token time

--- src/helpers/auth/auth/index.ts
+++ src/helpers/auth/auth/index.ts
@@ -3,7 +3,7 @@ export const generatejWT = (uid = '', role="user") => {
     return new Promise((resolve, reject) => {
         const payload = { uid, role };
         jwt.sign(payload,`${process.env.SECRETORPRIVATEKEY}`, {
-            expiresIn: '1h'
+            expiresIn: '1m'
         }, (err, token) => {
             if (err) {
                 console.log(err);
@@ -19,7 +19,7 @@ export const generateRefreshJWT = (uid: any, role: any) => {
     return new Promise((resolve, reject) => {
         const payload = { uid, role };
         jwt.sign(payload,`${process.env.REFRESH_SECRETORPRIVATEKEY}`, {
-            expiresIn: '1d'
+            expiresIn: '2m'
         }, (err, tokenRefresh) => {
             if (err) {
                 console.log(err);
