9186ca55f9103024622ea211ef817fb6747e188b
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-02-14 12:34:33 -0300

    [fix] edit profile middleware issues

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -8,7 +8,7 @@ const router = Router();
 
 // router.post('/register', register.check, register.do)
 router.post('/login', login.check, login.do)
-router.put('/edit-profile', editProfile.do)
+router.put('/edit-profile', validateJWT, editProfile.do)
 router.post('/token', refreshTokenFunc.do)
 router.get('/me',validateJWT, me.do)
 router.post('/facebook-login', facebookLogin.do)
