21bbf4250108321145e57c0a64f7ae06d891ca64
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-03-31 23:20:23 -0300

    [add] redis env variables

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,34 +2,52 @@
 import { Server as SocketIOServer, Socket } from "socket.io";
 import { Server } from "http";
 import { createClient } from "redis";
-import { OnlineUser, SocketUser } from "../types/express";
+import { SocketUser } from "../types/express";
 import { findTargetUser, handleLogin, handleLogout } from "./helpers";
 
-export const redisClient = createClient();
+export const redisClient = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: 12778
+    }
+});
 
-redisClient
-  .on("error", (err) => console.log("Redis Client Error", err))
+(async () => {
+  // Connect to redis server
+  await redisClient.connect();
+  
+})();
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
 
 export const socketHandler = (server: Server): SocketIOServer => {
   const io = new SocketIOServer(server);
 
   io.on("connection", (socket: Socket) => {
-    socket.on("set-turn", async (data: { barber: SocketUser; turnData: any }) => {
-      const targetBarber = await findTargetUser(data.barber._id, redisClient);
-      if (targetBarber) {
-        io.to(targetBarber.socketId).emit("add-turn", { data: data.turnData });
+    socket.on(
+      "set-turn",
+      async (data: { barber: SocketUser; turnData: any }) => {
+        const targetBarber = await findTargetUser(data.barber._id, redisClient);
+        if (targetBarber) {
+          io.to(targetBarber.socketId).emit("add-turn", {
+            data: data.turnData,
+          });
+        }
       }
-    });
+    );
 
     socket.on("log-in", async (data: { user: SocketUser }) => {
-        
       await handleLogin(data, socket, redisClient);
     });
 
     socket.on("remove-online-barber", (data: { user: SocketUser }) => {
-        handleLogout(data.user, redisClient)
+      handleLogout(data.user, redisClient);
     });
   });
 
   return io;
 };
+
+redisClient.quit();
